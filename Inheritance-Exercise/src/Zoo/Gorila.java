36331a4451b46de0b47559fc0346a27b79d67d6a
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-10-01 17:28:23 +0300

    Rearrange tasks .

--- Inheritance-Exercise/src/Zoo/Gorila.java
+++
@@ -1,7 +0,0 @@
-package Zoo;
-
-public class Gorila extends Mammal {
-    public Gorila(String name) {
-        super(name);
-    }
-}
--- Inheritance-Exercise/src/hero/Main.java
+++
@@ -1,9 +0,0 @@
-package hero;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Hero hero = new DarkWizard("Ivan", 5);
-        System.out.println(hero);
-    }
-}
