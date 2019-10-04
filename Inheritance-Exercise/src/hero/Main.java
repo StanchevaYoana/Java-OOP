61e86429edfdcb041a65953fdf9fe0e20bc4aa6a
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-10-04 16:13:47 +0300

    Rearrange task .

---
+++ Inheritance-Exercise/src/hero/Main.java
@@ -0,0 +1,9 @@
+package hero;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BladeKnight bladeKnight = new BladeKnight("New Hero",25);
+        System.out.println(bladeKnight.toString());
+    }
+}
