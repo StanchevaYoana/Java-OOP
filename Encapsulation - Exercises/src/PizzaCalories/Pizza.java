03395be3c4c110119189d9f163542fb66a4203b0
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-10-04 15:18:21 +0300

    Rearrange task .

--- Encapsulation - Exercises/src/PizzaCalories/Pizza.java
+++ Encapsulation - Exercises/src/PizzaCalories/Pizza.java
@@ -7,24 +7,26 @@ public class Pizza {
     private String name;
     private Dough dough;
     private List<Topping> toppings;
+    private int numberOfToppings;
 
     public Pizza(String name, int numberOfToppings) {
         this.setName(name);
         this.setToppings(numberOfToppings);
-    }
-
-    private void setName(String name) {
-        if (name.trim().isEmpty() || name.length() > 15) {
-            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
-        }
-        this.name = name;
+        this.toppings = new ArrayList<>(this.numberOfToppings);
     }
 
     private void setToppings(int numberOfToppings) {
         if (numberOfToppings < 0 || numberOfToppings > 10) {
             throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
         }
-        this.toppings = new ArrayList<>(numberOfToppings);
+        this.numberOfToppings = numberOfToppings;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty() || name.length() > 15) {
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
+        }
+        this.name = name;
     }
 
     public void setDough(Dough dough) {
@@ -36,7 +38,9 @@ public class Pizza {
     }
 
     public void addTopping(Topping topping) {
-        this.toppings.add(topping);
+        if(this.toppings.size() < this.numberOfToppings){
+            this.toppings.add(topping);
+        }
     }
 
     public double getOverallCalories() {
